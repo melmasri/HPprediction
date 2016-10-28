c873abe1d2d29d4b11153d00c4c024707869794a
Mohamad Elmasri <elmasri.m@gmail.com>
2016-10-28 13:56:33 +0200

    type in ana.table in weighted

--- main.10foldCV-weighted.R
+++ main.10foldCV-weighted.R
@@ -47,7 +47,7 @@ res = mclapply(1:tot.gr ,function(x, pairs, Z, dist, hyper){
     roc = rocCurves(Z=Z, Z_cross= com_paCross, P=P, plot=FALSE, bins=400, all=FALSE)
     tb  = ana.table(Z, com_paCross, roc=roc, plot=FALSE)
     roc.all = rocCurves(Z=Z, Z_cross= com_paCross, P=P, plot=FALSE, bins=400, all=TRUE)
-    tb.all  = ana.table(Z, com_paCross, roc=roc.all, plobtt=FALSE)
+    tb.all  = ana.table(Z, com_paCross, roc=roc.all, plot=FALSE)
     list(param=aux, tb = tb, tb.all = tb.all, FPR.all = roc.all$roc$FPR, TPR.all=roc.all$roc$TPR, FPR = roc$roc$FPR, TPR=roc$roc$TPR)
 },pairs=pairs,Z = com, dist=phy_dist, hyper=hyper, mc.preschedule = TRUE, mc.cores = tot.gr) 
 
