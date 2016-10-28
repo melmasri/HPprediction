469245b1f8bb9bfeaa77beb4dc61c5a32570a3e8
Mohamad Elmasri <elmasri.m@gmail.com>
2016-10-28 14:37:16 +0200

    updaing create databases for EID with updated RData files

--- create_datasets.R
+++ create_datasets.R
@@ -1,4 +1,12 @@
 ## Replicating numbers in Paper
+### Dataset created
+## GMP 
+## GMP single
+## GMP year
+## GMP year single
+## EID
+## EID single
+
 ## GMP
 rm(list=ls())
 source('library.R')
@@ -38,8 +46,6 @@ aux = which(rowSums(1*(com>0))==0);aux  #should be none
 aux = which(colSums(1*(com>0))==0);aux  #should be none
 aux = which(colSums(1*(com>0))==1);aux  #should be none
 
-
-
 com=lof(com)
 
 dim(com)
@@ -51,50 +57,8 @@ plot_Z(1*(com>0))
 
 save(com, phy_dist, file='comGMPD.RData')
 
-## EID
-rm(list=ls())
-source('library.R')
-source('loading data_EID.R')
-source('load_phyDistance.R')
-dim(com)
-dim(phy_dist)
-
-## Removing
-## rows with less than 2 interactions
-min.no= 2
-
-dim(com)
-#empty columns
-aux = which(colSums(1*(com>0))<min.no)
-if(length(aux)>0)
-    com = com[, -aux]
-dim(com)
-
-aux = which(rowSums(1*(com>0))==0);aux  #should be none
-if(length(aux)>0){
-    com = com[-aux,]
-    phy_dist= phy_dist[-aux, ]
-    phy_dist = phy_dist[, -aux]
-}
-
-
-
-aux = which(rowSums(1*(com>0))==0);aux  #should be none
-aux = which(colSums(1*(com>0))==0);aux  #should be none
-aux = which(colSums(1*(com>0))==1);aux  #should be none
-
-com=lof(com)
-
-dim(com)
-dim(phy_dist)
-range( phy_dist)/(max(phy_dist)+1e-2)
-phy_dist= phy_dist/(max(phy_dist)+1e-2)
-plot_Z(1*(com>0))
-
-save(com, phy_dist, file='comEID-PS.RData')
-
-
-## GMP - subset
+##################################################
+## GMP - year single
 rm(list=ls())
 source('library.R')
 source('loading data_GMPD_subset.R')
@@ -129,8 +93,8 @@ plot_Z(1*(com>0))
 save(com, phy_dist,pan, file='comGMPD-year.single.RData')
 
 
-
-## GMP - subset - nosingle
+##################################################
+## GMP - year 
 rm(list=ls())
 source('library.R')
 source('loading data_GMPD_subset.R')
@@ -166,8 +130,8 @@ plot_Z(1*(com>0))
 
 save(com, phy_dist,pan, file='comGMPD-year.RData')
 
-
-## EID - subset
+##################################################
+## EID
 rm(list=ls())
 source('library.R')
 source('loading data_EID_subset.R')
@@ -208,4 +172,52 @@ range( phy_dist)/(max(phy_dist)+1e-2)
 phy_dist= phy_dist/(max(phy_dist)+1e-2)
 plot_Z(1*(com>0))
 
-save(com, phy_dist, pan, file='comEID-PS-subset.single.RData')
+save(com, phy_dist, pan, file='comEID-PS.single.RData')
+
+##################################################
+## EID non-single
+rm(list=ls())
+source('library.R')
+source('loading data_EID_subset.R')
+source('load_phyDistance.R')
+dim(com)
+dim(phy_dist)
+
+
+## Removing columns with less than 2 interactions
+aux = which(colSums(1*(com>0))<2)
+if(length(aux)>0){
+    com = com[, -aux]
+}
+dim(com)
+
+## removing empty rows
+aux = which(rowSums(1*(com>0))==0)
+if(length(aux)>0){
+    com = com[-aux, ]
+    phy_dist = phy_dist[-aux,]
+    phy_dist = phy_dist[,-aux]
+}
+dim(com)
+
+## removing empty columns if any
+aux = which(colSums(1*(com>0))==0)
+if(length(aux)>0){
+    com = com[, -aux]
+}
+dim(com)
+
+# All these should be 0
+aux = which(rowSums(1*(com>0))==0);aux  #should be none
+aux = which(colSums(1*(com>0))==0);aux  #should be none
+aux = which(colSums(1*(com>0))==1);aux  #should be none
+
+# left ordering
+com=lof(com)
+
+# 
+range( phy_dist)/(max(phy_dist)+1e-2)
+phy_dist= phy_dist/(max(phy_dist)+1e-2)
+plot_Z(1*(com>0))
+
+save(com, phy_dist, pan, file='comEID-PS.RData')
