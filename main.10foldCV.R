b844e7eca21ff00c21a3cbd5de8ccf54ace4df91
Mohamad Elmasri <elmasri.m@gmail.com>
2016-12-18 23:57:36 +0100

    typo

--- main.10foldCV.R
+++ main.10foldCV.R
@@ -49,7 +49,7 @@ res = mclapply(1:tot.gr ,function(x, pairs, Z, dist, hyper){
     roc.all = rocCurves(Z=Z, Z_cross= com_paCross, P=P, plot=FALSE, bins=400, all=TRUE)
     tb.all  = ana.table(Z, com_paCross, roc=roc.all, plot=FALSE)
     list(param=aux, tb = tb, tb.all = tb.all, FPR.all = roc.all$roc$FPR, TPR.all=roc.all$roc$TPR, FPR = roc$roc$FPR, TPR=roc$roc$TPR)
-},pairs=pairs,Z = com_pa, dist=phy_dist,hyper=hyper, mc.preschedule = TRUE, mc.cores = 3) 
+},pairs=pairs,Z = com_pa, dist=phy_dist,hyper=hyper, mc.preschedule = TRUE, mc.cores = tot.gr) 
 
 if(SAVE_PARAM)
     save.image(file = 'param.RData')
