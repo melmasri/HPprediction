97c90f2914df760d6960dab9faf5931516ae13f3
Mohamad Elmasri <elmasri.m@gmail.com>
2017-08-22 17:05:43 +0200

    cleaning main

--- main.all.R
+++ main.all.R
@@ -8,31 +8,16 @@ library(geiger)
 ## loading tree
 source('example/download_tree.R')
 
-## source('library.R')
-## source('gen.R')
+## loading data
+source()
 
-
-tree <- read.tree('../Data/mammals.tre')
-tree <- drop.tip(tree, tree$tip.label[!tree$tip.label %in% rownames(com)])
-
-
+## sourcing MCMC script
+source('networkMCMC.R')
 
 param = network_est(Z = com, slices=5, tree=tree, model.type='both')
 
-    
-if(SAVE_PARAM)
-    save.image(file = 'param.RData')
+
+
+
 ##################################################
 ##################################################
-aux = getMean(param$param)
-pdist = 1/cophenetic(rescale(param$input$tree, 'EB', aux$eta))
-diag(pdist)<-0
-db = pdist%*%param$input$Z
-P = 1- exp(-outer(aux$y, aux$w)*db)
-
-ana.plot(param$param)
-roc = rocCurves(param$input$Z,param$input$Z,plot=TRUE,P,bins=400, all=TRUE)
-
-    dim(outer(aux$y, aux$w))
-dim(cophenetic(rescale(param$input$tree, 'EB', aux$eta)))
-plot_Z(1*(P>roc$thres))
