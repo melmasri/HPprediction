4cb75c6dcbd00d3d99ab6bba4ed5a5d4d1617570
Mohamad Elmasri <elmasri.m@gmail.com>
2017-09-04 14:28:08 +0200

    added a network_clean function to harmonize tree and Z

--- network_MCMC.R
+++ network_MCMC.R
@@ -14,6 +14,47 @@ network_est<-function(Z, slices = 10, tree = NULL, model.type = c('full', 'dista
     model.type= model.type[1]
     ## General warnings are checks
     if(missing(Z)) stop('Interaction matrix is missing!')
+    if(slices==0)  stop('no. of slices cannot be 0!')
+
+    cleaned = network_clean(Z, tree, model.type)
+    Z = cleaned$Z
+    tree = cleaned$tree
+
+    ## For affinity-only model 
+    if(grepl('aff', model.type,ignore.case = TRUE)){
+        print('Running affinity model...')
+        param = fullJoint_est(Z, iter = slices, uncertainty = uncertainty, ...)
+    }
+    ##  Full and distance model
+    if(grepl('(dist|full)', model.type)){
+        ## Running the MCMC
+        print(paste0('Running ',
+                     ifelse(grepl('dist', model.type),
+                            'distance model...', 'full model...')))
+        param  = ICM_est(unname(Z),tree,slices, distOnly = grepl('dist', model.type),
+            uncertainty = uncertainty, ...)
+    }
+    list(param =param , Z = Z, tree=tree)
+}
+
+network_clean<-function(Z, tree = NULL, model.type = c('full', 'distance', 'affinity')){
+    ## A function to clean Z and tree as:
+    ## - converts Z to binary
+    ## - removed empty columns from Z
+    ## - confirms tree is a phylo object when full or distance models
+    ## - removed tree tips that do not exist in the rows of Z
+    ## - removes the rows of Z with no corresponding tree tips
+    ## - orders the rows of Z abiding to cophenetic conversion.
+    
+    require(geiger)
+    ## Running options:
+    ## 1 - Full to combined 2 and 3;
+    ## 2 - Affinity-only model (affinity);
+    ## 3 - distance-only model (distance).
+
+    model.type= model.type[1]
+    ## General warnings are checks
+    if(missing(Z)) stop('Interaction matrix is missing!')
     if(!all(range(Z)==c(0,1))){
         warning('Z is converted to binary!', immediate. = TRUE, call.= FALSE)
         Z = 1*(Z>0)
@@ -21,24 +62,16 @@ network_est<-function(Z, slices = 10, tree = NULL, model.type = c('full', 'dista
     if(any(colSums(Z)==0)){
         stop('Z has empty columns, please remove!', immediate. = TRUE, call.= FALSE)
         Z = Z[,which(colSums(Z)>0)]
-    } 
-    if(slices==0)
-        stop('no. of slices cannot be 0!')
-
-    ## For affinity-only model 
+    }
     if(grepl('aff', model.type,ignore.case = TRUE)){
         if(!is.null(tree))
             warning('affinity model is chosen; ignoring tree!',
                     immediate. = TRUE, call. = FALSE)
-        print('Running affinity model...')
-        param = fullJoint_est(Z, iter = slices, uncertainty = uncertainty, ...)
-        return (list(param=param, Z = Z))
     }
-    ##  Full and distance model
     if(grepl('(dist|full)', model.type)){
         if(is.null(tree))
             stop('distance model is chosen, but tree is null!')
-
+        ## testing tree is a phylo object
         if(!is.phylo(tree))
             stop('tree must be a phylogeny tree, see gieger!')
 
@@ -58,21 +91,16 @@ network_est<-function(Z, slices = 10, tree = NULL, model.type = c('full', 'dista
         if(!all(sapply(sapply(rownames(Z),
                               function(r) which(r==tree$tip.label)), length)==1))
             stop('some row-species in Z exist more than once tree!')
-
+        
         ## Making sure the order of hosts in Z and tree are the same
-        row.order <- sapply(rownames(cophenetic(tree)),
-                            function(r) which(r==rownames(Z)))
-        print('Ordering the rows of Z to match tree...')
-        Z = Z[row.order,]
-
-        ## Running the MCMC
-        print(paste0('Running ',
-                     ifelse(grepl('dist', model.type),
-                            'distance model...', 'full model...')))
-        param  = ICM_est(unname(Z),tree,slices, distOnly = grepl('dist', model.type),
-            uncertainty = uncertainty, ...)
-        return(list(param = param,tree=tree, Z=Z))
+        if(!all(rownames(rownames(cophenetic(tree)) ==rownames(Z)))){
+            row.order <- sapply(rownames(cophenetic(tree)),
+                                function(r) which(r==rownames(Z)))
+            print('Ordering the rows of Z to match tree...')
+            Z = Z[row.order,]
+        }
     }
+    list(Z = Z, tree = tree)
 }
 
 ICM_est<-function(Z, tree, slices = 10, distOnly = FALSE, uncertainty = FALSE, ...){
