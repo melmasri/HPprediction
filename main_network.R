74516b02d5a2e266d7be6778dffad2f5d80bfb4e
Mohamad Elmasri <elmasri.m@gmail.com>
2017-09-05 15:35:32 +0200

    typo

--- main_network.R
+++ main_network.R
@@ -3,7 +3,7 @@
 #########################################
 
 ## General variables
-## please uncooment specify the following parameters
+## please specify the following parameters
 SAVE_PARAM = TRUE                    # should workspace be saved
 SAVE_FILE = 'param.RData'            # name of output R workspace file
 MODEL = 'full'                       # full, distance or affinity
