48df89e78599b16b75614cdb8f17f6f4c4563cd9
Mohamad Elmasri <elmasri.m@gmail.com>
2018-02-09 16:20:46 +0100

    added tol.err to division in rExp2

--- network_MCMC.R
+++ network_MCMC.R
@@ -378,7 +378,7 @@ rExp2<-function(l, g, Z, Z0, Z00){
     unif = matrix(runif(length(l)), dim(p))
     U = 1 + 0*p
     U[-Z0] = -log(1- unif[-Z0]*p[-Z0])/(l[-Z0] + tol.err)
-    aa = Z00 && (unif < g*p/(g*p + 1-p))
+    aa = Z00 && (unif < g*p/(tol.err + g*p + 1-p))
     U[aa] =  -log(1 - unif[aa]*(g*p[aa] + 1-p[aa])/g)/l[aa]
     U
 }
@@ -510,13 +510,11 @@ cophFast<-function(tree, lowerIndex, upperIndex, n){
     b
 }
 
-
 rEta.copheneticFast<-function(eta.old,tree,tree.ht,pdist.old, no0,i, sZ, Z, ywU,
                               eta_sd =0.01, a, b,nr, ind, sparse){
     ## a faster version of rEta using faster cophenetic and sparse matrices
     change = FALSE
     eta.prop = eta.old + eta_sd*rnorm(1)
-    eta.prop = sign(eta.prop)*min(abs(eta.prop),200)
     dist = cophFast(eb.phylo(tree, tree.ht, eta.prop), a, b,nr)
     pdist.new = dist[i,]%*%sZ
     if(sparse)
