2a094df67812e6aa283984a03a6c51fc8ccffd21
Mohamad Elmasri <elmasri.m@gmail.com>
2018-02-12 13:53:11 +0100

    fixed && to & in rExp2, need to re run uncertainty model

--- network_MCMC.R
+++ network_MCMC.R
@@ -378,7 +378,7 @@ rExp2<-function(l, g, Z, Z0, Z00){
     unif = matrix(runif(length(l)), dim(p))
     U = 1 + 0*p
     U[-Z0] = -log(1- unif[-Z0]*p[-Z0])/(l[-Z0] + tol.err)
-    aa = Z00 && (unif < g*p/(tol.err + g*p + 1-p))
+    aa = Z00 & (unif < g*p/(tol.err + g*p + 1-p))
     U[aa] =  -log(1 - unif[aa]*(g*p[aa] + 1-p[aa])/g)/l[aa]
     U
 }
