924fb1db83e74ba7b428ad97fdfc8fe95bfb1cb7
Mohamad Elmasri <elmasri.m@gmail.com>
2018-03-08 12:02:30 +0100

    mle.max=10

--- mainCV_network_latentnet.R
+++ mainCV_network_latentnet.R
@@ -119,9 +119,5 @@ pdf(paste0(subDir, 'tree_input.pdf'))
 plot(tree, show.tip.label=FALSE)
 dev.off()
 
-## Saving workspace
-if(SAVE_PARAM)
-    save.image(file = paste0(subDir, SAVE_FILE))
-
 ##################################################
 ##################################################
