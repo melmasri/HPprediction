766760c6db4ad0806f95f566810449ef28d7d720
Mohamad Elmasri <elmasri.m@gmail.com>
2019-03-30 18:22:41 -0400

    network_analysis from master

--- network_analysis.R
+++ network_analysis.R
@@ -10,12 +10,16 @@ plot_Z<-function(Z, xlab, ylab, ...){
     ## ploting interaction matrix as a binary image
     if(missing(ylab)) ylab = 'hosts'
 	if(missing(xlab)) xlab = 'parasites'
-    ## par(mar=c(4.5,4.5,1,1))
+    par(mar = c(5,5,1,1)+0.1)
 	image(1:ncol(Z), 1:nrow(Z), t(Z[nrow(Z):1,]),
 		col = c('white', 'black'), ylab=ylab, xlab=xlab,
-          useRaster=TRUE,srt=45, axes=FALSE,cex.lab=1.8)
-	axis(1, at = 100*0:(ceiling(ncol(Z)/100)), cex.axis=1.5)
-	axis(2, at = c(1,50*1:(ceiling(nrow(Z)/50))), labels = c(50*(ceiling(nrow(Z)/50)):1,1), cex.axis=1.5)
+          useRaster=TRUE,srt=45, axes=FALSE,cex.lab=3)
+           
+
+    a = 100*max(1,round(ncol(Z)*0.25 /100,0))
+	axis(1, at = a*0:(ceiling(ncol(Z)/a)), cex.axis= 2)
+    b = 100*max(1,round(nrow(Z)*0.25 /100,0))
+    axis(2, at = b*0:(ceiling(nrow(Z)/b)),cex.axis = 2)
 }
 
 lof<-function(Z, indices = FALSE){
@@ -110,23 +114,20 @@ cross.validate.fold<-function(Z, n= 10, min.per.col = 1, missing.pattern=c('rand
     gr = rep(size, n)
     if(sum(colm) %% size!=0)
         gr[n] =  gr[n] + sum(colm) %% size
-
-    pair.list = list()
-    for(i in 1:n){
-        bank=c()
-        for(k in 1:gr[i]){
-            a = which(colm>0)
-            if(missing.pattern=='random')
-                b = a[sample(length(a),1)] else
-            if (missing.pattern=='prop.to.col.sums')
-                b = a[sample(length(a),1, prob=colm[a]/sum(colm[a]))] else
-            stop('missing pattern has to be specified from selection!')
-            bank = c(bank, b)
-            colm[b] = colm[b]-1
-        }
-        pair.list[[i]]<-bank
+    
+    group.colm = rep(1:n,times = gr)[sample.int(sum(colm), sum(colm))]
+    pair.list = numeric(sum(colm))
+    for(i in 1:sum(colm)){
+        a = which(colm>0)
+        if(missing.pattern=='random')
+            b = a[sample.int(length(a),1)] else
+        if (missing.pattern=='prop.to.col.sums')
+            b = a[sample.int(length(a),1, prob=colm[a]/sum(colm[a]))] else
+        stop('missing pattern has to be specified from selection!')
+        colm[b] = colm[b]-1
+        pair.list[i]<-b
     }
-
+    pair.list= tapply(pair.list, group.colm,identity)
     
     gr.list= list()
     bank= c()
@@ -218,36 +219,91 @@ plot_degree <- function(Z, Z_est, type='both', host.col='blue', parasite.col='re
     }
     gpch = c('+', '*')
     if(type=='parasites'){
-        plot((para_degrees), type="p", col=parasite.col, pch=gpch[2], log="xy", xlim=xlim, ylim=ylim, ylab="Number of Nodes", xlab="Degree")
-        legend(xlim[2]*0.15, ylim[2]*0.6, c("Parasites"), col = parasite.col,
-               pch = gpch[2], box.col="white")
+        plot((para_degrees), type="p", col=parasite.col, pch=gpch[2], log="xy", xlim=xlim, ylim=ylim, ylab="Number of nodes", xlab="Degree", cex.lab = 1.5,cex.axis = 1.5)
+        legend(xlim[2]*0.03, ylim[2]*1.4, c("Parasites"), col = parasite.col,
+               pch = gpch[2], bty="n",pt.cex=1.5, cex=2)
         if(!missing(Z_est)){
             points((para_est), type="p", col=parasite.col, pch=16)
-            legend(xlim[2]*0.15, ylim[2]*0.4, c("Est"), col = parasite.col,
-               pch = 16, box.col="white")
+            legend(xlim[2]*0.03, ylim[2]*0.8, c("Est"), col = parasite.col,
+               pch = 16, bty='n', pt.cex=1.5, cex=2)
         }
     }
     if(type=='hosts'){
-        plot((host_degrees), type="p", col=host.col, pch=gpch[1], log="xy", xlim=xlim, ylim=ylim, ylab="Number of Nodes", xlab="Degree")
-        legend(xlim[2]*0.15, ylim[2]*0.6, c("Hosts"), col = host.col,
-               pch = gpch[1], box.col="white")
+        plot((host_degrees), type="p", col=host.col, pch=gpch[1], log="xy", xlim=xlim, ylim=ylim, ylab="Number of nodes", xlab="Degree", cex.lab = 1.5,cex.axis = 1.5)
+        legend(xlim[2]*0.03, ylim[2]*1.4, c("Hosts"), col = host.col,
+               pch = gpch[1], bty="n", pt.cex=1.5, cex=2)
         if(!missing(Z_est)){
             points((host_est), type="p", col=host.col, pch=16)
-            legend(xlim[2]*0.15, ylim[2]*0.4, c("Est"), col = host.col,
-                   pch = 16, box.col="white")
+            legend(xlim[2]*0.03, ylim[2]*0.8, c("Est"), col = host.col,
+                   pch = 16, bty='n',pt.cex=1.5, cex=2)
         }
     }
     if(type=='both'){
-        plot((para_degrees), type="p", col=parasite.col, pch=gpch[2], log="xy", xlim=xlim, ylim=ylim, ylab="Number of Nodes", xlab="Degree")
+        plot((para_degrees), type="p", col=parasite.col, pch=gpch[2], log="xy", xlim=xlim, ylim=ylim, ylab="Number of nodes", xlab="Degree", cex.lab = 1.5,cex.axis = 1.5)
         points((host_degrees), type="p", col=host.col, pch=gpch[1])
-    legend(xlim[2]*0.15, ylim[2]*0.6, c("Parasites", "Hosts"), col = c(parasite.col, host.col),
-           pch = gpch[2:1], box.col="white")
+    legend(xlim[2]*0.03, ylim[2]*1.4, c("Parasites", "Hosts"), col = c(parasite.col, host.col),
+           pch = gpch[2:1], bty = 'n', pt.cex=1.5, cex=2)
         if (!missing(Z_est)) {
             points((para_est), type="p", col=parasite.col, pch=16)
             points((host_est), type="p", col=host.col, pch=16)
-            legend(xlim[2]*0.15, ylim[2]*0.33, c("Est"), col = c("black"),
-                   pch = 16, box.col="white")
+            legend(xlim[2]*0.03, ylim[2]*0.33, c("Est"), col = c("black"),
+                   pch = 16, bty='n', pt.cex=1.5, cex=2)
         }
     }
 }
 
+sample_parameter<-function(param, MODEL,Z, tree, size = 1000, weights=NULL){
+    sample_mcmc<-function(mcmc_sample,nObs,  size =1000){
+        if(is.matrix(mcmc_sample))
+            mcmc_sample[, sample.int(nObs, size, replace = TRUE)] else
+        mcmc_sample[sample.int(nObs, size, replace = TRUE)]
+    }
+    if(!is.null(weights) & length(weights)!=size)
+        stop('weights are not the same sampling size.')
+    t.max = get.max.depth(tree)
+    dist.original = unname(cophenetic(rescale(tree, 'EB', 0)))/2
+      if(grepl('dist', MODEL)) {
+        Y = W = 1
+    }else{
+        Y = sample_mcmc(param$y, ncol(param$y), size)
+        W = sample_mcmc(param$w, ncol(param$w), size)
+    }
+    if(grepl('(dist|full)', MODEL)){
+        Eta = sample_mcmc(param$eta, length(param$eta), size)
+    }else Eta = 1
+
+    
+    zeroZ = which(Z>0)
+    P <- 0
+    for(s in 1:size){
+        ## aux = sapply(1:size, function(s){
+        ## setting affinity to 1 in distance model
+        if(grepl('(aff|full)', MODEL)){
+            YW = outer(Y[,s], W[,s])
+        } else YW = 1
+        ## Full or distance model
+        ## Creating distance
+        if(grepl('(full|dist)', MODEL)){
+            distance = 1/EB.distance(dist.original, t.max, Eta[s])
+            diag(distance)<-0
+            distance = distance %*% Z
+            distance[distance==0] <- if(grepl('dist', MODEL)) Inf else 1
+        } else distance = 1
+        ## models
+        ## P = 1-exp(-outer(Y, W))                 # affinity model
+        ## P = 1-exp(-distance)                    # distance model
+        ## P = 1-exp(-YW*distance)                 # full model
+        ## All in one probability matrix
+        if(!is.null(weights)){
+            a =  1-  exp(-YW*distance)
+            Pg = a * weights[s] /(1-a + weights[s] * a)
+            Pg[zeroZ] <- a[zeroZ]
+            P = P + Pg
+        }else{
+            P = P + 1-  exp(-YW*distance)
+        }
+    }
+    ## })
+    matrix(P/size, nrow = nrow(com), ncol = ncol(com))
+
+}
