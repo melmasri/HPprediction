ecad81382e94e3b46c254879ae19949aa7d1912a
Mohamad <mo@julia>
2020-10-15 12:25:17 -0400

    fixed minor bugs and improved sampling function

--- R/network_clean.R
+++ R/network_clean.R
@@ -9,7 +9,7 @@
 #' 
 #'
 #' @param Z bipartite interaction matrix. Rows should correspond to species in the tree. There should be no empty columns.
-#' @param tree 'phylo' object
+#' @param tree 'phylo' object, or an symmetric non-negative matrix of pairwise distance between the rows of Z
 #' @param model.type Indicate model to use: one of 'full', 'distance', 'affinity'
 #'
 #' @return Returns a list of the cleaned Z and tree objects
@@ -46,91 +46,85 @@ function(Z, tree = NULL, model.type = c('full', 'distance', 'affinity')){
     }
     if(grepl('aff', model.type)){
         if(!is.null(tree))
-            warning('affinity model is chosen; ignoring tree!',
+            warning('affinity model is chosen; tree can be used as a constant if parameter constant_distance = TRUE!, otherwise ignored',
                     immediate. = TRUE, call. = FALSE)
     }
     if(grepl('(dist|full)', model.type)){
         if(is.null(tree))
             stop('distance model is chosen, but tree is null!')
-        ## testing tree is a phylo object
-        if(!is.phylo(tree) & !(isSymmetric(tree) & min(tree) >=0)){
-            stop('tree must be a phylo object! or a symmetric matrix of non-negative pairwise distances between the rows of Z')
+    }
+    ## testing tree is a phylo object
+    if(!is.phylo(tree) & !(isSymmetric(tree) & min(tree) >=0)){
+        stop('tree must be a phylo object! or a symmetric matrix of non-negative pairwise distances between the rows of Z')
+    }
+    ## removing any column of Z that has 0 interactions
+    if(any(colSums(Z)==0)){
+        print('Z has empty columns - these have been removed!')
+        Z = Z[,which(colSums(Z)>0)]
+    }
+    ## testing that all tips exist in Z
+    if(is.phylo(tree)){
+        if(!all(tree$tip.label %in% rownames(Z))){
+            warning('not all species in tree exist in Z; missing are removed from tree!',
+                    immediate.= TRUE, call. = FALSE)
+            tree = drop.tip(tree, tree$tip.label[!(tree$tip.label %in% rownames(Z))])
         }
-        ## testing that all tips exist in Z
-        if(is.phylo(tree)){
-            if(!all(tree$tip.label %in% rownames(Z))){
-                warning('not all species in tree exist in Z; missing are removed from tree!',
-                        immediate.= TRUE, call. = FALSE)
-                tree = drop.tip(tree, tree$tip.label[!(tree$tip.label %in% rownames(Z))])
-            }
-            ## Testing all names in com exist in dist
-            if(!all(rownames(Z) %in% tree$tip.label)){
+        ## Testing all names in com exist in dist
+        if(!all(rownames(Z) %in% tree$tip.label)){
             warning('not all rows in Z exist tree; missing are removed from Z!',
                     immediate.= TRUE, call. = FALSE)
             Z = Z[rownames(Z) %in% tree$tip.label,]
-            }
-            
-        }else{
-            if(!all(rownames(tree) %in% rownames(Z))){
-                warning('not all row names in tree exist in Z; missing are removed from tree!',
-                        immediate.= TRUE, call. = FALSE)
-                aux= (rownames(tree) %in% rownames(Z))
-                tree = tree[aux, aux ]
-            }
-        
-            if(!all(rownames(Z) %in% rownames(tree))){
-                warning('not all rows in Z exist tree; missing are removed from Z!',
-                        immediate.= TRUE, call. = FALSE)
-                aux = which(rownames(Z) %in% rownames(tree))
-                if(length(aux)>0)
-                    Z = Z[aux,]
-                else
-                    stop('No rows of Z exist in rows of tree!')
-            }
         }
-        if(any(colSums(Z)==0)){
-            print('Z has empty columns - these have been removed!')
-            Z = Z[,which(colSums(Z)>0)]
-        }
+
         ## Testing that names in Z exist only once
-        if(is.phylo(tree)){
-            if(!all(sapply(sapply(rownames(Z),
-                                  function(r) which(r==tree$tip.label)), length)==1))
-                stop('some row-species in Z exist more than once tree!')
-            
-            ## Making sure the order of hosts in Z and tree are the same
-            aux  = cophenetic(tree)
-            if(!all(rownames(aux)==rownames(Z))){
-                row.order <- sapply(rownames(aux),function(r) which(r==rownames(Z)))
-                print('ordering the rows of Z to match tree...')
-                Z = Z[row.order,]
-            }
-            if(max(range(tree$edge.length))>1){
-                print('normalizing tree edges by the maximum pairwise distance!')
-                tree$edge.length = tree$edge.length/(max(aux)/2)
-            }
-        }else{
-            if(!all(sapply(sapply(rownames(Z),
-                                  function(r) which(r==rownames(tree))), length)==1))
-                stop('some row-species in Z exist more than once in rownames of distance!')
-            
-            ## Making sure the order of hosts in Z and tree are the same
-            aux  = tree
-            if(!all(rownames(aux)==rownames(Z))){
-                row.order <- sapply(rownames(aux),function(r) which(r==rownames(Z)))
-                print('ordering the rows of Z to match tree...')
-                Z = Z[row.order,]
-            }
-            if(max(tree)>1){
-                print('normalizing distance edges by the maximum pairwise distance!')
-                tree = tree/max(aux)
-            }
-            if(min(tree) < 0){
-                stop('minimum distance is negative!, input must be non-negative')
-            }
-            if(!isSymmetric(tree)){
-                stop('non symmetrics distances are not allowed!')
-            }
+        if(!all(sapply(sapply(rownames(Z),
+                              function(r) which(r==tree$tip.label)), length)==1))
+            stop('some row-species in Z exist more than once tree!')
+        
+        ## Making sure the order of hosts in Z and tree are the same
+        aux  = cophenetic(tree)
+        if(!all(rownames(aux)==rownames(Z))){
+            row.order <- sapply(rownames(aux),function(r) which(r==rownames(Z)))
+            print('ordering the rows of Z to match tree...')
+            Z = Z[row.order,]
+        }
+        if(max(range(tree$edge.length))>1){
+            print('normalizing tree edges by the maximum pairwise distance!')
+            tree$edge.length = tree$edge.length/(max(aux)/2)
+        }
+    }else{
+        if(!all(rownames(tree) %in% rownames(Z))){
+            warning('not all row names in tree exist in Z; missing are removed from tree!',
+                    immediate.= TRUE, call. = FALSE)
+            aux= (rownames(tree) %in% rownames(Z))
+            tree = tree[aux, aux ]
+        }
+        if(!all(rownames(Z) %in% rownames(tree))){
+            warning('not all rows in Z exist tree; missing are removed from Z!',
+                    immediate.= TRUE, call. = FALSE)
+            aux = which(rownames(Z) %in% rownames(tree))
+            if(length(aux)>0)
+                Z = Z[aux,]
+            else
+                stop('No rows of Z exist in rows of tree!')
+        }
+        if(!all(sapply(sapply(rownames(Z),
+                              function(r) which(r==rownames(tree))), length)==1))
+            stop('some row-species in Z exist more than once in rownames of distance!')
+        
+        ## Making sure the order of hosts in Z and tree are the same
+        aux  = tree
+        if(!all(rownames(aux)==rownames(Z))){
+            row.order <- sapply(rownames(aux),function(r) which(r==rownames(Z)))
+            print('ordering the rows of Z to match tree...')
+            Z = Z[row.order,]
+        }
+        if(max(tree)>1){
+            print('normalizing distance edges by the maximum pairwise distance!')
+            tree = tree/max(aux)
+        }
+        if(min(tree) < 0){
+            stop('minimum distance is negative!, input must be non-negative')
         }
     }
     list(Z = Z, tree = tree)
